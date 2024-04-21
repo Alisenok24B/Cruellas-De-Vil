64411c777340f22b39c30e003bb15c68f0cf1008
Julya Shapaeva <shapaeva03@mail.ru>
2024-04-21 23:41:50 +0300

    Add styles to CustomSection

---
+++ src/components/custom-section/custom-section.styled.ts
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled"
+
+
+export const StyledCustomSectionPlace = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+        
+    margin-right: 120px;
+
+    @media only screen and (max-width: 1024px) {
+        margin-right: 0;
+        margin-bottom: 20px;
+    }
+`
+
+export const StyledCustomSectionSort = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media only screen and (max-width: 1024px) {
+        margin-bottom: 20px;
+    }
+`
--- src/components/custom-section/custom-section.tsx
+++ src/components/custom-section/custom-section.tsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React from "react"
+
 import { Inp } from '../../components/inp/inp'
+import { StyledCustomSectionPlace } from "./custom-section.styled"
+import { StyledCustomSectionSort } from "./custom-section.styled"
 
 export function CustomSection (props) {
     return (
         <section className={`${props.className}`}>
             {props.type === 'text' && (
-                <>
+                <StyledCustomSectionPlace>
                     <h1>{ props.children }</h1>
                     <Inp type={`${props.type}`} name="where-find" placeholder="Страна, регион, город" />
-                </>
+                </StyledCustomSectionPlace>
             )}
             {props.type === 'select' && (
-                <>
+                <StyledCustomSectionSort>
                     <h1>{ props.children }</h1>
                     <Inp type={`${props.type}`} placeholder="Сортировать по:" options={[
                             { value: "ascending", label: "Возрастанию цены" },
                             { value: "descending", label: "Убыванию цены" }]} />
-                </>
+                </StyledCustomSectionSort>
             )}
         </section>
     )
