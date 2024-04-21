316213e27670c8b174630abaac6f99a34406a890
Julya Shapaeva <shapaeva03@mail.ru>
2024-04-21 23:42:42 +0300

    Add styles to Menu

---
+++ src/components/menu/menu.styled.ts
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled"
+
+
+export const StyledMenu = styled.ul`
+    display: flex;
+    align-items: center;
+
+    margin-left: auto;
+`
+
+export const StyledMenuLi = styled.li`
+    margin-right: 40px;
+`
+
+export const StyledMenuProfile = styled.a`
+    display: none;
+`
+
+export const StyledMenuShowProfile = styled.a`
+    display: block;
+`
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React from "react"
+
 import { map } from '../../assets/img'
 import { profile } from '../../assets/img'
 
+import { StyledMenu } from "./menu.styled"
+import { StyledMenuLi } from "./menu.styled"
+import { StyledMenuProfile } from "./menu.styled"
+import { StyledMenuShowProfile } from "./menu.styled"
+
 export function Menu(props) {
     return (
-        <ul className={`${props.className}`}>
-            <li>
+        <StyledMenu className={`${props.className}`}>
+            <StyledMenuLi>
                 <a href="#">
                     <img src={map} alt="Картинка в виде карты" />
                 </a>
-            </li>
-            <li>
-                <a href="#" id="profile" className="show-profile">
-                    <img src={profile} alt="Картинка в виде человека" />
-                </a>
-            </li>
-            <li>
+            </StyledMenuLi>
+            <StyledMenuLi>
+                {props.className.includes("show-profile") ? (
+                    <StyledMenuShowProfile href="#" id="profile">
+                        <img src={profile} alt="Картинка в виде человека" />
+                    </StyledMenuShowProfile>
+                ) : (
+                    <StyledMenuProfile href="#" id="profile">
+                        <img src={profile} alt="Картинка в виде человека" />
+                    </StyledMenuProfile>
+                )}
+            </StyledMenuLi>
+            <StyledMenuLi>
                 <a href="#">
                     Выход
                 </a>
-            </li>
-        </ul>
+            </StyledMenuLi>
+        </StyledMenu>
     );
 }
