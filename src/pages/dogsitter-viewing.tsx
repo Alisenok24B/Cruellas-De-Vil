335a543deed9fa26db80acaa954fdeb65ddffbc5
sonya21adammali <sonyashamsutdinova531@gmail.com>
2024-04-27 11:27:31 +0300

    profile page added

---
+++ src/pages/dogsitter-viewing.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { HeaderElements } from '../components/header-elements/header-elements'
+import { Container } from '../components/container'
+import { logo, user_photo } from '../assets/img';
+import { Contacts } from '../components/contacts';
+import { Logo } from '../components/logo';
+import { UserPhoto } from '../components/user-photo';
+import { UserData } from '../components/user-data';
+import { UserInfo } from '../components/user-info';
+import { UserAbout } from '../components/user-about';
+
+const ProfileView = () => {
+    return (
+        <>
+        <header id="header" className="header">
+        <Container className="container">
+            <HeaderElements className="nav" src={logo} alt="Человек с собакой"/>
+        </Container>
+        </header>
+
+        <main id="main" className="main">
+            <UserData className="name" name="Имя Фамилия" src={user_photo} alt="Фото пользователя"/>
+            <UserInfo className="info"
+                location="Советский район"
+                price="1500 руб/день"
+                phone="+79393967375"
+            />
+            <UserAbout title="Обо мне">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Quisque nec eleifend nulla. Sed elementum sagittis lectus,
+                non finibus nulla tristique et. Proin dignissim porta mauris,
+                at posuere tellus malesuada in. Nulla facilisi. Mauris dapibus
+                libero in velit aliquet, sed ullamcorper justo eleifend.
+            </UserAbout>
+        </main>
+
+        <footer id="footer" className="footer">
+        <Container className="container">
+            <Logo className="logo" src={logo} alt="Человек с собакой" title="DogSittersFinder — любовь и забота о вашей собаке"/>
+            <Contacts className="contacts">+7 800 555-35-35</Contacts>
+        </Container>
+    </footer>
+    </>
+    );
+}
+
+export default ProfileView;
