3fd8b51332af302f700c4b6168308c9e91ff9a0f
Alice Bikson <alisabikson@gmail.com>
2024-05-03 22:25:58 +0300

    Добавлена ;

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Login from './pages/login';
 import Register from './pages/register';
-import Search from './pages/search'
+import Search from './pages/search';
 
 const App = () => {
   return (
