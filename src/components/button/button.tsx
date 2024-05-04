ac10021b4ee32c6268a873f99f5bca12e0a11abf
Alice Bikson <alisabikson@gmail.com>
2024-05-04 11:46:53 +0300

    Убраны лишние пробелы

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,10 +3,9 @@ import { StyledButton, Icon } from './button.styled';
 
 export function Button (props) {
     return (
-            <StyledButton type={props.type} isGoogle={props.isGoogle}>
-                {props.isGoogle && props.icon && <Icon src={props.icon} alt="Google" />}
-                {props.children}
-            </StyledButton>
- 
+        <StyledButton type={props.type} isGoogle={props.isGoogle}>
+            {props.isGoogle && props.icon && <Icon src={props.icon} alt="Google" />}
+            {props.children}
+        </StyledButton>
     );
 }
