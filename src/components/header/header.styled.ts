f7b52fdb256390cd218966b6a78c3aed202adfdb
Julya Shapaeva <shapaeva03@mail.ru>
2024-05-11 02:23:00 +0300

    Add styles for header

---
+++ src/components/header/header.styled.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled"
+
+
+export const StyledHeader = styled.header`
+    background: #FFFFFF;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+
+    padding-top: 20px;
+    padding-bottom: 20px;
+`
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,10 +5,14 @@ import { Container } from '../container';
 
 import { logo } from '../../assets/img';
 
+import { StyledHeader } from "./header.styled";
+
 export function Header(props) {
     return (
-        <Container>
-            <HeaderElements src={logo} alt="Человек с собакой"/>
-        </Container>
+        <StyledHeader>
+            <Container>
+                <HeaderElements src={logo} alt="Человек с собакой"/>
+            </Container>
+        </StyledHeader>
     );
 }
