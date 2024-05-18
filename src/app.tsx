566a5db6c703968e8aee72b3da154f9c6457bebd
Julya Shapaeva <shapaeva03@mail.ru>
2024-05-18 10:20:14 +0300

    Add globalStyles for pages

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { PageRoutes } from "./routes";
 
+import { globalStyles } from "./global-styles";
+import { Global } from "@emotion/react";
+
 const App = () => {
   return (
     <BrowserRouter>
+      <Global styles={globalStyles} />
       <PageRoutes/>
     </BrowserRouter>
   );
