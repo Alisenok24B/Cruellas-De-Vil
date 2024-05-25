1bf024ca221882d1e392f8730616c5a06283a96d
Julya Shapaeva <shapaeva03@mail.ru>
2024-05-25 20:26:13 +0300

    Remove className

--- src/components/input-select/input-select.tsx
+++ src/components/input-select/input-select.tsx
@@ -5,7 +5,7 @@ import { StyledInpSelect } from "./input-select.styled"
 
 export function InputSelect(props) {
     return (
-        <StyledInp className="inp">
+        <StyledInp>
             <StyledInpSelect value={props.value} onChange={props.onChange}>
                 <option value="" disabled selected>{props.placeholder}</option>
                 {props.options.map((option, index) => (
