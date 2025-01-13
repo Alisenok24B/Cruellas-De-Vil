3633af37ab994b13ff611a2d67acb98c20455b82
Julya Shapaeva <shapaeva03@mail.ru>
2025-01-14 01:16:22 +0300

    Update StyledMain styles

--- src/pages/search.styled.ts
+++ src/pages/search.styled.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const StyledMain = styled.main`
     padding-top: 40px;
     padding-bottom: 40px;
+    min-width: 576px;
 `
 
 export const StyledFind = styled.div`
