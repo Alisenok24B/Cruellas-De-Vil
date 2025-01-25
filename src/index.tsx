217c7a5adc308d30a4c6aac1d038169026d06f00
Julya Shapaeva <shapaeva03@mail.ru>
2025-01-25 16:29:56 +0300

    Add localization

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/display-name */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import i18next from 'i18next';
+import { i18nextReactInitConfig } from '@brojs/cli';
+// import { i18nextReactInitConfig } from './i18next';
+
+i18next.t = i18next.t.bind(i18next)
+const i18nextPromise = i18nextReactInitConfig(i18next)
 
 const clientId = '866643980887-43d7m5qkq33283vh2pldk53mkmuok5om.apps.googleusercontent.com';
 
@@ -15,11 +24,14 @@ export default Main;
 
 let rootElement: ReactDOM.Root;
 
-export const mount = (Component, element = document.getElementById('app')) => {
+export const mount = async (Component, element = document.getElementById('app')) => {
   rootElement = ReactDOM.createRoot(element);
+  await i18nextPromise
   rootElement.render(<Component />);
-
+  // @ts-ignore
   if (module.hot) {
+    // @ts-ignore
+    await i18next.reloadResources()
     module.hot.accept('./app', () => {
       rootElement.render(<Component />);
     });
